8808e83e1f69bd41457f2d758e295a0b5107c52a
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-17 21:15:24 +0300

    somple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 """
 module for calculating a pagination.
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     function for calculating a pagination.
     """
