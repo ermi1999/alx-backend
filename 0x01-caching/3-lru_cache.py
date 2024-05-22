fdf09a9d58f30cd914784d35afd927f2cae555c9
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-22 23:58:30 +0300

    lru caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+basic LRU caching system.
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    class for creating a LRU caching system.
+    """
+    def __init__(self):
+        """
+        initializer
+        """
+        super().__init__()
+        self.__lru_table = {}
+
+    def put(self, key, item):
+        """
+        inserts an item to cache_data following the rule of LRU.
+        """
+        if not key or not item:
+            return
+
+        if (len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS
+                and not self.cache_data.get(key)):
+            key_to_delete = next(iter(self.__lru_table))
+            print(f"DISCARD: {key_to_delete[0]}")
+            del self.cache_data[key_to_delete]
+            del self.__lru_table[key_to_delete]
+        if self.cache_data.get(key):
+            del self.cache_data[key]
+        if key in self.__lru_table.keys():
+            del self.__lru_table[key]
+        if len(self.__lru_table) > 1:
+            self.__lru_table[key] = len(self.__lru_table) - 1
+        else:
+            self.__lru_table[key] = 0
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        gets a value associated with the key.
+        """
+        if key not in self.cache_data.keys():
+            return None
+        if key in self.__lru_table.keys():
+            del self.__lru_table[key]
+        if len(self.__lru_table) > 1:
+            self.__lru_table[key] = len(self.__lru_table) - 1
+        else:
+            self.__lru_table[key] = 0
+        return self.cache_data.get(key, None)
