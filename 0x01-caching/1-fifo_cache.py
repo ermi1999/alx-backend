2be68da92d748b1eeacfb9ec7a0632c5d88f8bbf
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-22 11:04:56 +0300

    fifo caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+basic FIFO caching system.
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    class for creating a FIFO caching system.
+    """
+    def put(self, key, item):
+        """
+        inserts an item to cache_data following the rule of FIFO.
+        """
+        if not key or not item:
+            return
+
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            key_to_delete = next(iter(self.cache_data))
+            print(f"DISCARD: {key_to_delete}")
+            del self.cache_data[key_to_delete]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        gets a value associated with the key.
+        """
+        return self.cached_data.get(key)
