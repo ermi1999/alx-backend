11b452a67fafdb2bb1a8a39e01b58de0cd0e22d6
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-24 19:53:18 +0300

    Track progress and errors with Kue: Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,19 @@
+import kue from "kue";
+
+const jobData = { phoneNumber: "0910245656", message: "notification pushed" };
+
+const queue = kue.createQueue({ name: "push_notification_code" });
+
+const job = queue.create("push_notification_code", jobData).save((err) => {
+  if (!err) {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
+
+job.on("complete", () => {
+  console.log("Notification job completed");
+});
+
+job.on("failed", () => {
+  console.log("Notification job failed");
+});
