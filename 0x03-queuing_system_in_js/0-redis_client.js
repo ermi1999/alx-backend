08fe1627093604f196be2471e274d8a28e2d9806
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-24 11:54:08 +0300

    promisify redis request

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,7 +3,7 @@ import redis from "redis";
 const client = redis.createClient();
 
 client.on("error", (err) =>
-  console.log("Redis client not connected to the server: ", err)
+  console.log(`Redis client not connected to the server: ${err}`)
 );
 
 client.on("connect", () => console.log("Redis client connected to the server"));
--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -4,7 +4,7 @@ import { promisify } from "util";
 const client = redis.createClient();
 
 client.on("error", (err) =>
-  console.log("Redis client not connected to the server: ", err)
+  console.log(`Redis client not connected to the server: ${err}`)
 );
 
 client.on("connect", () => console.log("Redis client connected to the server"));
