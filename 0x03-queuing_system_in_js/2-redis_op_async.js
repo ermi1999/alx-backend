d57ebdc4a1b11eb48da6cf100da86ce0d77d5750
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-24 11:47:17 +0300

    promisify redis request

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,24 @@
+import redis from "redis";
+import { promisify } from "util";
+
+const client = redis.createClient();
+
+client.on("error", (err) =>
+  console.log("Redis client not connected to the server: ", err)
+);
+
+client.on("connect", () => console.log("Redis client connected to the server"));
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+async function displaySchoolValue(schoolName) {
+  const asynced = promisify(client.get).bind(client);
+  const value = await asynced(schoolName);
+  console.log(value);
+}
+
+displaySchoolValue("Holberton");
+setNewSchool("HolbertonSanFrancisco", "100");
+displaySchoolValue("HolbertonSanFrancisco");
